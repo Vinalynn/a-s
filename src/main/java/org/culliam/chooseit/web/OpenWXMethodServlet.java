6644c5b4470b9003229e52b0f461a83bda8c1f62
Vinalynn <william.cai@live.com>
2013-04-15 21:52:47 +0800

    添加微信 Token

--- src/main/java/org/culliam/chooseit/web/OpenWXMethodServlet.java
+++ src/main/java/org/culliam/chooseit/web/OpenWXMethodServlet.java
@@ -10,8 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -86,6 +85,21 @@ public class OpenWXMethodServlet extends HttpServlet{
         if (log.isInfoEnabled()) {
             log.info(userRequest);
         }
+        try{
+            String reqString = HttpUtils.dump(req.getInputStream(), "UTF-8");
+            if(log.isInfoEnabled()){
+                log.info(reqString);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        //BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+
+
+
 
     }
 
